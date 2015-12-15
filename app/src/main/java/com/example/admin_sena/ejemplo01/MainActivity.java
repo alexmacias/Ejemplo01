2f6de1b68cffbbcf88da46678638fe562c1c5e9b
borisgr04@hotmail.com <admin_sena@SENA>
2015-12-15 11:44:08 -0500

    Cree variable prueba

--- app/src/main/java/com/example/admin_sena/ejemplo01/MainActivity.java
+++ app/src/main/java/com/example/admin_sena/ejemplo01/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText txtNombre;
     private Button btnAceptar;
+    private Button btnAcepta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
